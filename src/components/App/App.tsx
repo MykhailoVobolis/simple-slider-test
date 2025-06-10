381930278799053b45d6fc547448e029f7257213
MykhailoVobolis <mvobolis@gmail.com>
2025-06-11 00:53:35 +0300

    deleted unnecessary files

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,5 @@
 import TestimonialsPage from "../../pages/TestimonialsPage/TestimonialsPage";
 
-import css from "./App.module.css";
-
 export default function App() {
   return <TestimonialsPage />;
 }
